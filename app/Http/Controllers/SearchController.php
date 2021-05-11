aab27a24f5743491f035f382509efbff22264ea7
Abid Rakhmansyah <bidnth@gmail.com>
2021-05-11 22:01:26 +0700

    vue

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Scout\Searchable;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    use Searchable;
+    //
+}
---
+++ resources/js/components/SearchComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <ais-index app-id="VKSP2DHMZK" api-key="89c3c15cb85a34d2b87edfc354e0b994" index-name="dev_ecohome">
+        <ais-search-box></ais-search-box>
+        <ais-results inline-template>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Customer</th>
+                        <th scope="col">Customer Address</th>
+                        <th scope="col">Rep Name</th>
+                        <th scope="col">Solar Company</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="result in results" :key="result.objectID">
+                        <th scope="row">{{ result.objectID }}</th>
+                        <td>{{ result.date }}</td>
+                        <td>{{ result.customerName }}</td>
+                        <td>{{ result.customerAddress }}</td>
+                        <td>{{ result.homeRep }}</td>
+                        <td>{{ result.solarCompany }}</td>
+                        <td>
+                            <div class="d-grid gap-2">
+                            <button type="button" class="btn btn-success">Edit Report</button>
+                            <button type="button" class="btn btn-danger">Delete Report</button>
+                            </div>
+                            </td>
+                    </tr>
+                </tbody>
+            </table>
+        </ais-results>
+        <ais-pagination v-bind:class-names="{
+            'ais-pagination': 'pagination',
+            'ais-pagination-item': 'page-item',
+            'ais-pagination-link': 'page-link'
+            }"></ais-pagination>
+    </ais-index>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
