4a40cf0ec217b24dab4748d6d9f63a6dafae3d6c
Abid Rakhmansyah <bidnth@gmail.com>
2021-05-11 10:25:59 +0700

    add button

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <head>
   <meta charset="UTF-8">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/instantsearch.css@7.4.5/themes/satellite-min.css" integrity="sha256-TehzF/2QvNKhGQrrNpoOb2Ck4iGZ1J/DI4pkd2oUsBc=" crossorigin="anonymous">
 </head>
 <body>
@@ -15,15 +16,15 @@
 
   <script type="text/html" id="hit-template">
     <div class="hit">
-      <p class="hit-name">
+      <!-- <p class="hit-name">
         @verbatim
         {{#helpers.highlight}}{ "attribute": "customerName" }{{/helpers.highlight}}
         {{#helpers.highlight}}{ "attribute": "customerAddress" }{{/helpers.highlight}}
         @endverbatim
-      </p>
+      </p> -->
     </div>
   </script>
-
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/algoliasearch@4.5.1/dist/algoliasearch-lite.umd.js" integrity="sha256-EXPXz4W6pQgfYY3yTpnDa3OH8/EPn16ciVsPQ/ypsjk=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/instantsearch.js@4.8.3/dist/instantsearch.production.min.js" integrity="sha256-LAGhRRdtVoD6RLo2qDQsU2mp+XVSciKRC8XPOBWmofM=" crossorigin="anonymous"></script>
   <script src="/assets/js/search.js"></script>
