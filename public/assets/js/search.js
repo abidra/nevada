df8af6302c4541aa74c6a0b60c9a9435e5a305dd
Abid Rakhmansyah <bidnth@gmail.com>
2021-05-11 10:27:42 +0700

    update instantsearch

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -27,10 +27,34 @@ const searchClient = algoliasearch(
     instantsearch.widgets.hits({
       container: '#hits',
       templates: {
-        item: document.getElementById('hit-template').innerHTML,
+        item: `
+        <div>
+          <p>Name: {{#helpers.highlight}}{ "attribute": "customerName"}{{/helpers.highlight}}</p>
+          <p>Address: {{#helpers.highlight}}{ "attribute": "customerAddress"}{{/helpers.highlight}}</p>
+          <button type="button" class="btn btn-success">Edit report</button>
+          <button type="button" class="btn btn-danger">Delete report</button>
+        </div>
+      `,
         empty: `We didn't find any results for the search <em>"{{query}}"</em>`,
       },
     })
   ]);
+
+  search.addWidget({
+    render: function(opts) {
+      const results = opts.results;
+      // read the hits from the results and transform them into HTML.
+      document.querySelector('#hits').innerHTML = results.hits.map(function(h) {
+        return '<p>' + h._highlightResult.title.value + '</p>';
+      }).join('');
+    }
+  });
+//   search.sortBy({
+//     container: '#sort-by',
+//     items: [
+//       { label: 'Unsorted', value: 'instant_search' },
+//       { label: 'Sorted', value: 'instant_search_price_asc' },
+//     ],
+//   });
   
   search.start();
